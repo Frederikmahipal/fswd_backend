c386c9b0906ff9c92b3c27a0e1b79b8b4ac24bf5
Frederikmahipal <frederikmhv@gmail.com>
2024-03-12 13:08:32 +0100

    ...

--- routes/company_routes.js
+++ routes/company_routes.js
@@ -4,12 +4,22 @@ import company from '../models/company_models.js';
 import roleCheck from '../middleware/role_middleware.js';
 import authJwt from '../middleware/auth.js';
 
+
+router.get('/all', authJwt, roleCheck(['admin', 'employee']), async (req, res) => {
+    try {
+        const companies = await company.find({});
+        res.status(200).json(companies);
+    } catch (error) {
+        console.error('Error getting companies:', error);
+        res.status(500).json({ message: 'Server error', error: error.message });
+    }
+});
+
 router.post('/create', authJwt, roleCheck(['admin']), async (req, res) => {
     try {
         const { company_name, company_address, company_city, company_country, company_email } = req.body;
 
         const existingCompany = await company.findOne({ company_name });
-
         if (existingCompany) {
             return res.status(400).json({ message: 'Company already exists' });
         }
