5d9c2e6ec20f73537f11485d73ea13f573813585
Frederikmahipal <frederikmhv@gmail.com>
2024-03-12 22:06:33 +0100

    ...

--- db/db.js
+++ db/db.js
@@ -5,6 +5,7 @@ dotenv.config();
 
 export const connectDB = async () => {
     try {
+      console.log(process.env.MONGO_URI);
       await mongoose.connect(process.env.MONGO_URI, {
         useNewUrlParser: true,
         useUnifiedTopology: true,
